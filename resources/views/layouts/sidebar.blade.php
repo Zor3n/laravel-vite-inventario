9bf0357c555b22d1d712ca54c65914fb289d6f4a
Renacuajo <braurrego@protonmail.com>
2022-07-28 12:49:56 -0500

    New routes added

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -75,7 +75,7 @@
         @if (Auth::user()->id_rol === 1)
             <ul class="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
                 <li>
-                    <a href="{{ route('create') }}"
+                    <a href="{{ route('resume') }}"
                         class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                         <svg aria-hidden="true"
                             class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -88,7 +88,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('create') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <a href="{{ route('users') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                        <span class="flex-1 ml-3 whitespace-nowrap">{{ __('Personal') }}</span>
                     </a>
