92ba30ec9b54362e348314010fb70397a360a5be
Renacuajo <braurrego@protonmail.com>
2022-07-27 21:59:15 -0500

    First push: Routes and views configurated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title }} : {{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Flowbite -->
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.1/dist/flowbite.min.css" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="font-sans antialiased">
+    <div class="flex flex-row min-h-screen bg-gray-100">
+        <!-- Sidebar layout -->
+        @include('layouts.sidebar')
+        <!-- Page Content -->
+        <main id="main-content" class="flex-1 ml-64 transition-all duration-500 ease-in-out">
+            <!-- Navbar layout -->
+            @include('layouts.navigation')
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+            <!-- Page Content -->
+            <div class="main-content flex flex-col flex-grow p-4">
+
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+    <style>
+        @media (max-width: 639px) {
+            #sidebar-menu {
+                display: none;
+            }
+
+            #main-content {
+                margin-left: 0;
+            }
+
+            #logo-nav {
+                display: inline;
+            }
+
+            #sidebar-toggle-conteiner {
+                display: none;
+            }
+        }
+    </style>
+    <!-- Flowbite script -->
+    <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
+    <!-- Scripts -->
+    <script>
+        function sidebarToggle() {
+            document.getElementById("sidebar-menu").classList.toggle("translate-y-full");
+            document.getElementById("sidebar-menu").classList.toggle("invisible");
+            document.getElementById("main-content").classList.toggle("ml-64");
+        }
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,99 @@
+{{-- sidebar fixed h-screen w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-gray-800 --}}
+<aside id="sidebar-menu" class="w-64 fixed left-0 top-0 h-screen bg-gray-800 overflow-y-auto transition-all duration-500 ease-in-out" aria-label="Sidebar">
+    <div class="py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+        <ul class="space-y-2">
+            {{-- sidebar header --}}
+            <li>
+                <a class="flex justify-center pl-2.5 mb-5 bg-gray-700 rounded-full py-2" href="{{ route('dashboard') }}">
+                    <x-application-logo class="block h-10 w-auto fill-current text-white" />
+                </a>
+            </li>
+            {{-- sidebar facturación --}}
+            <li>
+                <button type="button"
+                    class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    aria-controls="fac-menu-dropdown" data-collapse-toggle="fac-menu-dropdown">
+                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap"
+                        sidebar-toggle-item>{{ __('Facturación') }}</span>
+                    <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <ul id="fac-menu-dropdown" class="hidden py-2 space-y-2">
+                    <li>
+                        <a href="{{ route('create') }}"
+                            class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">{{ __('Crear') }}</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('search') }}"
+                            class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">{{ __('Buscar') }}</a>
+                    </li>
+                </ul>
+            </li>
+            {{-- sidebar inventario --}}
+            <li>
+                <button type="button"
+                    class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    aria-controls="inv-menu-dropdown" data-collapse-toggle="inv-menu-dropdown">
+                    <svg aria-hidden="true"
+                        class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
+                        </path>
+                    </svg>
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap"
+                        sidebar-toggle-item>{{ __('Inventario') }}</span>
+                    <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <ul id="inv-menu-dropdown" class="hidden py-2 space-y-2">
+                    <li>
+                        <a href="{{ route('shoes') }}"
+                            class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">{{ __('Zapatos') }}</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('accesories') }}"
+                            class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">{{ __('Accesorios') }}</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        @if (Auth::user()->id_rol === 1)
+            <ul class="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
+                <li>
+                    <a href="{{ route('create') }}"
+                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg aria-hidden="true"
+                            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">{{ __('Resumen') }}</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('create') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                       <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                       <span class="flex-1 ml-3 whitespace-nowrap">{{ __('Personal') }}</span>
+                    </a>
+                 </li>
+            </ul>
+        @endif
+    </div>
+</aside>
